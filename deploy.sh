fc4125c1813da71cc82f61c76a5f6ce28dfb16ab
Julio Viera <julio.viera@gmail.com>
2024-11-29 11:00:24 -0500

    Deploy script fixes

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,12 @@ set -e
 VERSION="$1"
 
 if [ -z "$VERSION" ]; then
-        echo "Usage ./deploy.sh <version>"
-        exit 3
-    fi
-
-if [ "$OPENVPN_PORT_UDP" == "off" ];then
-    OPENVPN_PORT_UDP=
+    echo "Usage: ./deploy.sh <version>"
+    exit 3
 fi
 
-git tag v$VERSION
-docker buildx build --platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm/v8,linux/386 . -t julman99/openvpn-supereasy:$VERSION  -t julman99/openvpn-supereasy:latest --push
+git tag "v$VERSION"
+docker buildx build --platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/386 . \
+    -t "julman99/openvpn-supereasy:$VERSION" \
+    -t julman99/openvpn-supereasy:latest \
+    --push
